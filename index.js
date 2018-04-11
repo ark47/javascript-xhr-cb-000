22c28245ce7556099f7356689e9d198828066805
Chris Nunez <cenunez47@gmail.com>
2018-04-11 16:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,8 @@ function getCommits(el) {
   req.open("GET", 'https://api.github.com/repos/ark47/' + name + '/commits');
   req.send();
 }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+}
